fb1531ef5dcc7b556b43e0641524ade62b055fa2
Tiago Bortoletto Vaz <tiago@acaia.ca>
2016-06-27 19:27:01 -0400

    Fix settings.

--- lmhs/settings.py
+++ lmhs/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 MEDIA_ROOT = PROJECT_ROOT + '/media/'
 MEDIA_URL = '/media/'
+STATIC_ROOT='/static/'
+
 
 FIXTURE_DIRS = (
    'lmhsweb/fixtures/',
