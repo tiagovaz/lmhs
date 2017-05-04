d32830cbb6d4dcfd6df863e74a5cb4ea55c3c54f
Tiago Bortoletto Vaz <tiago@acaia.ca>
2017-05-04 20:45:31 +0000

    Clean filter.

--- lmhsweb/filters.py
+++ lmhsweb/filters.py
@@ -12,14 +12,8 @@ class MainFilter(django_filters.FilterSet):
     TYPE_CHOICES_FILTER = BLANK_CHOICE_DASH
     TYPE_CHOICES_FILTER.extend(list(TYPE_CHOICES))
 
-
-    #PROJECT_CHOICES = Projet.objects.all().values_list("id", "nom")
-    #PROJECT_CHOICES_FILTER = [('', 'Tous')]
-    #PROJECT_CHOICES_FILTER.extend(list(PROJECT_CHOICES))
-
     titre = django_filters.CharFilter(label="Titre", lookup_expr='icontains')
     auteur__nom = django_filters.CharFilter(label="Auteur", lookup_expr='icontains')
-#    projet__nom = django_filters.CharFilter(label="Projet", lookup_expr='icontains')
     mot_cle__nom = django_filters.CharFilter(label="Mot clé", lookup_expr='icontains')
     pdf_text = django_filters.CharFilter(label="Recherche PDF", lookup_expr='icontains')
     source = django_filters.CharFilter(label="Source", lookup_expr='icontains')
